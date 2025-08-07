5fe14df4bd62d8515d6d8f11650ce7d3e7fa2af8
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-08-07 13:14:02 -0400

    "react working"

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
-const para = React.createElement("p", {}, "React Basics by Nishal");
+/*
+<div id="parent">
+       <div id="child">
+         <h1></h1>
+       </div>
+</div>
+*/
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(para);
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+    React.createElement("h1",{className:"new"},"Hello World")
+    )
+);
+
+console.log(parent);//object
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
