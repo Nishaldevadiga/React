89fe9a124d58ed11121905db1ca757e955d856e0
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-08-08 11:49:45 -0400

    "jsx"

--- AppBasic.js
+++ AppBasic.js
@@ -1,20 +1,7 @@
-/*
-<div id="parent">
-       <div id="child">
-         <h1></h1>
-       </div>
-</div>
-*/
-
-import React from "react";
-
 import ReactDOM from "react-dom/client";
 
+//jsx
+const parent=<h1>Hello World</h1>;
 
-const parent = React.createElement("div", { id: "parent" }, "hello worlds"
-
-);
-
-console.log(parent);//object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
