21e10f52155594c09d9b9bd4a3f4d9263891ee3d
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-08-08 11:51:41 -0400

    "class"

--- AppBasic.js
+++ AppBasic.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 
 //jsx
-const parent=<h1>Hello World</h1>;
+const parent=<h1 className="new">Hello World</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
