651606e2cd42a0295f0d310f7e93b382c5e58065
Nishaldevadiga <65213089+Nishaldevadiga@users.noreply.github.com>
2025-08-08 12:16:02 -0400

    "functional compnent"

--- AppBasic.js
+++ AppBasic.js
@@ -1,10 +1,21 @@
 import ReactDOM from "react-dom/client";
 
-//jsx
+//jsx- React element
 const parent=(
 <h1 className="new" tabIndex="1">
   Hello World</h1>
   );
 
+//React Component- 2 types of Component
+
+//Class Based Component- OLD
+
+
+//Functional Component- NEW
+//javascript function returns a react element
+const heading=()=>{
+  return <h1>Pinkan</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading());
